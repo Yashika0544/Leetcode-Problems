8800fe923e94d69b2d1c2d38da44cc751bd2409d
Yashika0544 <119301739+Yashika0544@users.noreply.github.com>
2024-03-04 14:14:59 +0530

    H-Index.py

--- H-Index.py
+++ H-Index.py
@@ -1,1 +1,14 @@
-
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        citations.sort()
+        '''m=0
+        for i in range(len(citations)):
+            if citations[i]>len(citations)-1:
+                m=max(m,len(citations)-1)
+        return m
+        '''
+        m=len(citations)
+        for i in citations:
+            if i<m:
+                m-=1
+        return m
