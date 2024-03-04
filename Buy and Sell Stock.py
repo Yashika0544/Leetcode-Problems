28996836b76f43e3e4839c3eab41720dabcd4703
Yashika0544 <119301739+Yashika0544@users.noreply.github.com>
2024-03-04 14:13:14 +0530

    Buy and Sell Stock.py

--- Buy and Sell Stock.py
+++ Buy and Sell Stock.py
@@ -1,1 +1,15 @@
-
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        p=0
+        i=0
+        j=1
+        while j<len(prices):
+            cp=prices[j]-prices[i]
+            #if prices[i]>prices[j]:
+            #    break
+            if prices[i]<prices[j]:
+                p=max(cp,p)
+            else:
+                i=j
+            j+=1
+        return p
