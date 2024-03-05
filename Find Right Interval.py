c2d4c7ae0bb9b9c668096f5de2f5eabdbee9d93e
Yashika0544 <119301739+Yashika0544@users.noreply.github.com>
2024-03-05 18:01:10 +0530

    Find Right Interval.py

--- Find Right Interval.py
+++ Find Right Interval.py
@@ -1,1 +1,13 @@
-
+class Solution:
+    def findRightInterval(self, intervals: List[List[int]]) -> List[int]:
+        for index, interval in enumerate(intervals):
+            interval.append(index)
+        intervals.sort()
+        n = len(intervals)
+        answer = [-1] * n  # Initialize the answer list with -1
+        for _, end, o_index in intervals:
+            r_index = bisect_left(intervals, [end])
+            if r_index < n:
+                answer[o_index] = intervals[r_index][2]
+      
+        return answer 
