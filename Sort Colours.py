af9621af98bffc3edff957d21680068c8baa920e
Yashika0544 <119301739+Yashika0544@users.noreply.github.com>
2024-03-05 17:49:56 +0530

    Sort Colours.py

--- Sort Colours.py
+++ Sort Colours.py
@@ -1,1 +1,16 @@
-
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        n=len(nums)
+        l=0
+        r=n-1
+        curr=0
+        while(curr<=r):
+            if(nums[curr]==2):
+                nums[curr],nums[r]=nums[r],nums[curr]
+                r-=1
+            elif(nums[curr]==1):
+                curr+=1
+            else:
+                nums[curr],nums[l]=nums[l],nums[curr]
+                l+=1
+                curr+=1
