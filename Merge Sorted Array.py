15097c34753e6871ee754e137ae27f1a8a68ac6d
Yashika0544 <119301739+Yashika0544@users.noreply.github.com>
2024-03-05 12:06:47 +0530

    Merge Sorted Array.py

--- Merge Sorted Array.py
+++ Merge Sorted Array.py
@@ -1,1 +1,9 @@
-
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        c=0
+        while(m<len(nums1)):
+            nums1[m]=nums2[c]
+            c+=1
+            m+=1
+        nums1.sort()
+        
