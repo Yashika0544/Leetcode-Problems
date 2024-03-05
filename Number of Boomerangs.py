67659deccadf393c9641e60bed11016abc4647af
Yashika0544 <119301739+Yashika0544@users.noreply.github.com>
2024-03-05 12:07:39 +0530

    Number of Boomerangs.py

--- Number of Boomerangs.py
+++ Number of Boomerangs.py
@@ -1,1 +1,9 @@
-
+class Solution:
+    def numberOfBoomerangs(self, points: List[List[int]]) -> int:
+        ans=0
+        for x1,y1 in points:
+            c=collections.Counter()
+            for x2,y2 in points:
+                ans+=2*c[(x1-x2)**2 + (y1-y2)**2]
+                c[(x1-x2)**2 + (y1-y2)**2]+=1
+        return ans
