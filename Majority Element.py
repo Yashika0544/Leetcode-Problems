3e026620a7330ee789a51457a86e094d9a7b91f9
Yashika0544 <119301739+Yashika0544@users.noreply.github.com>
2024-03-05 12:05:23 +0530

    Majority Element.py

--- Majority Element.py
+++ Majority Element.py
@@ -1,1 +1,13 @@
-
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        c=0
+        mE=0
+        for i in range(len(nums)):
+            if c==0:
+                mE=nums[i]
+            if mE==nums[i]:
+                c+=1
+            else:
+                c-=1
+        return mE
+        
