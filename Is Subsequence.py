b865192d37704f012443428598d702973c5763d5
Yashika0544 <119301739+Yashika0544@users.noreply.github.com>
2024-03-05 11:58:01 +0530

    Is Subsequence.py

--- Is Subsequence.py
+++ Is Subsequence.py
@@ -1,1 +1,13 @@
-
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        s=list(s)
+        c=0
+        for i in t:
+            try:
+                if i==s[c]:
+                    c+=1
+            except:
+                continue
+        if c==len(s):
+            return True
+        return False
